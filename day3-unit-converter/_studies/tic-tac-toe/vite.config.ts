56b4135ee3b8cf043e4c092734945cc97221e0de
carloscsc <carlos.azarro@gmail.com>
2024-07-29 06:50:08 -0300

    Test for build folder

--- day3-unit-converter/_studies/tic-tac-toe/vite.config.ts
+++ day3-unit-converter/_studies/tic-tac-toe/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
-  
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "https://carloscsc.github.io/my-study-journal/day3-unit-converter/_studies/tic-tac-toe/dist",
+  plugins: [react()],
+});
