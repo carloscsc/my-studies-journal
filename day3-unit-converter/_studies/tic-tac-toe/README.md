3cb387a9edca204ad54a5db4f576361bbbc26bd7
Carlos <carlos.azarro@gmail.comm>
2024-07-29 20:11:38 -0300

    chore: Update Vite configuration for Tic-Tac-Toe project and the title of the app

--- day3-unit-converter/_studies/tic-tac-toe/README.md
+++ day3-unit-converter/_studies/tic-tac-toe/README.md
@@ -1,5 +1,11 @@
 # Tic-Tac-Toe
+
 [[see in action]](https://my-study-journal.vercel.app/day3-unit-converter/_studies/tic-tac-toe/dist)
 
 Project based on the tutorial: [React.dev/Learn/Tutorial: Tic-Tac-Toe](https://react.dev/learn/tutorial-tic-tac-toe)
 
+## P.S.
+
+I add the `dist` folder in the main branch, only to deploy it to Vercel
+in the most easiest way. In a real scenario, Is better to use a dedicated repo and
+a separated branch for the production code.
--- day3-unit-converter/_studies/tic-tac-toe/vite.config.ts
+++ day3-unit-converter/_studies/tic-tac-toe/vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/day3-unit-converter/_studies/tic-tac-toe/dist/",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  let base = "/";
+
+  if (command === "build") {
+    base = "/day3-unit-converter/_studies/tic-tac-toe/dist/";
+  }
+
+  return {
+    plugins: [react()],
+    base,
+  };
 });
