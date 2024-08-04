76b17b7965d4dd7a019caf09053a0db2df5cf05e
carloscsc <carlos.azarro@gmail.com>
2024-08-04 08:22:00 -0300

    Refactor: Switch from useState to useReducer
    - Replaced multiple useState hooks with a single useReducer
    - Centralized state management in a reducer function
    - Reduced re-renders and improved performance
    - Simplified useEffect hooks for state changes
    
    feat: Add Results component to be implemented

--- day3-unit-converter/src/App.jsx
+++ day3-unit-converter/src/App.jsx
@@ -1,91 +1,173 @@
-import { useEffect, useState } from "react";
+import { useReducer, useEffect } from "react";
 import MeasureTypesSelector from "./MeasureTypesSelector.jsx";
 import UnitSelector from "./UnitSelector.jsx";
+import Results from "./Results.jsx";
 
+/**
+ * Initial state of the application.
+ * @typedef {Object} initialState
+ * @property {Array} measureTypes - The list of available measure types.
+ * @property {Object} selectedType - The currently selected measure type.
+ * @property {Array} unitsToConvert - The list of units to convert.
+ * @property {Object} selectedUnitToConvert - The currently selected unit to convert.
+ * @property {Array} unitsToTarget - The list of units to target.
+ * @property {Object} selectedUnitToTarget - The currently selected unit to target.
+ * @property {number} inputConverter - The input value for conversion.
+ * @property {number} inputTarget - The converted value.
+ */
+const initialState = {
+  measureTypes: [],
+  selectedType: {},
+  unitsToConvert: [],
+  selectedUnitToConvert: {},
+  unitsToTarget: [],
+  selectedUnitToTarget: {},
+  inputConverter: 1,
+  inputTarget: 0,
+};
+
+/**
+ * Reducer function for managing state in the application.
+ *
+ * @param {Object} state - The current state of the application.
+ * @param {Object} action - The action object that describes the state change.
+ * @returns {Object} - The new state after applying the action.
+ */
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_MEASURE_TYPES":
+      return { ...state, measureTypes: action.payload, selectedType: action.payload[0] };
+    case "SET_SELECTED_TYPE":
+      return { ...state, selectedType: action.payload };
+    case "SET_UNITS_TO_CONVERT":
+      return { ...state, unitsToConvert: action.payload };
+    case "SET_SELECTED_UNIT_TO_CONVERT":
+      return { ...state, selectedUnitToConvert: action.payload };
+    case "SET_UNITS_TO_TARGET":
+      return { ...state, unitsToTarget: action.payload };
+    case "SET_SELECTED_UNIT_TO_TARGET":
+      return { ...state, selectedUnitToTarget: action.payload };
+    case "SET_INPUT_CONVERTER":
+      return { ...state, inputConverter: action.payload };
+    case "SET_INPUT_TARGET":
+      return { ...state, inputTarget: action.payload };
+    default:
+      return state;
+  }
+}
+
+/**
+ * Reducer hook for managing state in the App component.
+ *
+ * @param {function} reducer - The reducer function for updating state.
+ * @param {object} initialState - The initial state object.
+ * @returns {array} - An array containing the current state and a dispatch function.
+ */
 function App() {
-  const [measureTypes, setMeasureTypes] = useState([]);
-  const [selectedType, setSelectedType] = useState([]);
+  /**
+   * Reducer hook for managing state in the App component.
+   *
+   * @param {function} reducer - The reducer function for updating state.
+   * @param {object} initialState - The initial state object.
+   * @returns {array} - An array containing the current state and a dispatch function.
+   */
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [unitsToConvert, setUnitsToConvert] = useState([]);
-  const [selectedUnitToConvert, setSelectedUnitToConvert] = useState([]);
-
-  const [unitsToTarget, setUnitsToTarget] = useState([]);
-  const [selectedUnitToTarget, setSelectedUnitToTarget] = useState([]);
-
-  const [inputConverter, setInputConverter] = useState(1);
-  const [inputTarget, setInputTarget] = useState(0);
-
+  // Get initial data from db and set initial value for measureTypes
+  // and selectedMeasureTypes
   useEffect(() => {
-    fetch("/Data.json")
+    fetch("/data.json")
       .then((response) => response.json())
       .then((data) => {
-        setMeasureTypes(data.measureTypes);
-        setSelectedType(data.measureTypes[0]);
+        dispatch({ type: "SET_MEASURE_TYPES", payload: data.measureTypes });
       })
       // eslint-disable-next-line no-console
       .catch((error) => console.error("Error fetching the measure types:", error));
   }, []);
 
-  // chage the units every time that data change
+  // useEffect hook to update state when selectedType or inputConverter changes
+  // If selectedType has units, dispatch actions to set units and conversions in the state
   useEffect(() => {
-    if (selectedType.units) {
-      setUnitsToConvert(selectedType.units);
-      setUnitsToTarget(Object.entries(selectedType.units[0].conversions));
-      setSelectedUnitToConvert(selectedType.units[0]);
-      setSelectedUnitToTarget(Object.entries(selectedType.units[0].conversions)[0]);
-
-      setInputTarget(inputConverter * Object.entries(selectedType.units[0].conversions)[0][1]);
+    if (state.selectedType.units) {
+      const units = state.selectedType.units;
+      const conversions = Object.entries(units[0].conversions);
+      dispatch({ type: "SET_UNITS_TO_CONVERT", payload: units });
+      dispatch({ type: "SET_UNITS_TO_TARGET", payload: conversions });
+      dispatch({ type: "SET_SELECTED_UNIT_TO_CONVERT", payload: units[0] });
+      dispatch({ type: "SET_SELECTED_UNIT_TO_TARGET", payload: conversions[0] });
+      dispatch({ type: "SET_INPUT_TARGET", payload: state.inputConverter * conversions[0][1] });
     }
-  }, [selectedType]);
+  }, [state.selectedType, state.inputConverter]);
 
+  // useEffect hook to update state when selectedUnitToConvert or inputConverter changes
+  // If selectedUnitToConvert has conversions, dispatch actions to set conversions and input target in the state
   useEffect(() => {
-    if (selectedUnitToConvert.conversions) {
-      setUnitsToTarget(Object.entries(selectedUnitToConvert.conversions));
-      setSelectedUnitToTarget(Object.entries(selectedUnitToConvert.conversions)[0]);
-      setInputTarget(inputConverter * Object.entries(selectedUnitToConvert.conversions)[0][1]);
+    if (state.selectedUnitToConvert.conversions) {
+      const conversions = Object.entries(state.selectedUnitToConvert.conversions);
+      dispatch({ type: "SET_UNITS_TO_TARGET", payload: conversions });
+      dispatch({ type: "SET_SELECTED_UNIT_TO_TARGET", payload: conversions[0] });
+      dispatch({ type: "SET_INPUT_TARGET", payload: state.inputConverter * conversions[0][1] });
     }
-  }, [selectedUnitToConvert]);
+  }, [state.selectedUnitToConvert, state.inputConverter]);
 
+  // useEffect hook to update input target when selectedUnitToTarget or inputConverter changes
+  // If selectedUnitToTarget has a valid conversion factor, set input target based on conversion
+  // Otherwise, set input target to 0
   useEffect(() => {
-    if (selectedUnitToTarget[1]) {
-      setInputTarget(inputConverter * selectedUnitToTarget[1]);
+    if (state.selectedUnitToTarget[1]) {
+      dispatch({
+        type: "SET_INPUT_TARGET",
+        payload: state.inputConverter * state.selectedUnitToTarget[1],
+      });
     } else {
-      setInputTarget(0);
+      dispatch({ type: "SET_INPUT_TARGET", payload: 0 });
     }
-  }, [selectedUnitToTarget]);
+  }, [state.selectedUnitToTarget, state.inputConverter]);
 
+  /**
+   * Sets the value of the input and updates the converter and target values accordingly.
+   *
+   * @param {Event} e - The event object representing the input change event.
+   * @returns {void}
+   */
   function setValue(e) {
-    setInputConverter(e.target.value);
-    setInputTarget(e.target.value * selectedUnitToTarget[1]);
+    const value = e.target.value;
+    dispatch({ type: "SET_INPUT_CONVERTER", payload: value });
+    dispatch({ type: "SET_INPUT_TARGET", payload: value * state.selectedUnitToTarget[1] });
   }
 
   return (
     <>
-      <MeasureTypesSelector setType={setSelectedType} types={measureTypes} />
+      <MeasureTypesSelector
+        setType={(type) => dispatch({ type: "SET_SELECTED_TYPE", payload: type })}
+        types={state.measureTypes}
+      />
 
       <input
         type="number"
         className="input-converter"
         min={1}
-        value={inputConverter}
+        value={state.inputConverter}
         onChange={setValue}
       />
 
       <UnitSelector
-        units={unitsToConvert}
-        setUnit={setSelectedUnitToConvert}
-        selected={selectedUnitToConvert && selectedUnitToConvert.name}
+        units={state.unitsToConvert}
+        setUnit={(unit) => dispatch({ type: "SET_SELECTED_UNIT_TO_CONVERT", payload: unit })}
+        selected={state.selectedUnitToConvert && state.selectedUnitToConvert.name}
       />
 
-      <span> = </span>
-
-      <input type="text" className="input-target" value={inputTarget} readOnly />
+      <span> to </span>
 
       <UnitSelector
-        units={unitsToTarget}
-        setUnit={setSelectedUnitToTarget}
-        selected={selectedUnitToTarget && selectedUnitToTarget[0]}
+        units={state.unitsToTarget}
+        setUnit={(unit) => dispatch({ type: "SET_SELECTED_UNIT_TO_TARGET", payload: unit })}
+        selected={state.selectedUnitToTarget && state.selectedUnitToTarget[0]}
       />
+
+      {/* <Results output={state.inputTarget} /> */}
+
+      <input type="text" className="input-target" value={state.inputTarget} readOnly />
     </>
   );
 }
