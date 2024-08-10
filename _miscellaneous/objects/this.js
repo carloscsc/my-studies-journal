2353f47cd24cbeb55e1ae8abe2e3b6e88e7b7aa8
carloscsc <carlos.azarro@gmail.com>
2024-08-10 18:49:36 -0300

    add caining to this.js

--- _miscellaneous/objects/this.js
+++ _miscellaneous/objects/this.js
@@ -38,3 +38,22 @@ let arrowUser = {
 }
 
 arrowUser.sayHi() 
+
+
+/** Chaining */
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function() { // shows the current step
+    console.log(this.step)
+    return this;
+  }
+}
+ladder.up().up().down().showStep().down().showStep().up().showStep();
