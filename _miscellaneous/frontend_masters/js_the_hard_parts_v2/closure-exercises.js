0d476e86d86d37defd718a8a4c9e6d9e999b2c12
carloscsc <carlos.azarro@gmail.com>
2024-08-13 09:39:03 -0300

    add resolution for some closure exercises

--- _miscellaneous/frontend_masters/js_the_hard_parts_v2/closure-exercises.js
+++ _miscellaneous/frontend_masters/js_the_hard_parts_v2/closure-exercises.js
@@ -1,26 +1,33 @@
 // Exercises: http://csbin.io/closures
-// Response: https://github.com/FrontendMasters/fm-snippets/blob/main/javascript-hard-parts-v2/closures.js˝ß
+// Response: https://github.com/FrontendMasters/fm-snippets/blob/main/javascript-hard-parts-v2/closures.js
 
 /** CHALLENGE 1 */
 function createFunction() {
-
+  return () => "hello"
 }
 
 // /*** Uncomment these to check your work! ***/
-// const function1 = createFunction();
-// function1(); // => should console.log('hello');
+const function1 = createFunction();
+console.log(function1()); 
+// => should console.log('hello');
 
 
 /** CHALLENGE 2 */
 function createFunctionPrinter(input) {
-
+  return () => input
 }
 
 // /*** Uncomment these to check your work! ***/
-// const printSample = createFunctionPrinter('sample');
-// printSample(); // => should console.log('sample');
-// const printHello = createFunctionPrinter('hello');
-// printHello(); // => should console.log('hello');
+const printSample = createFunctionPrinter('sample');
+console.log(printSample())
+console.log(printSample())
+console.log(printSample())
+// => should console.log('sample');
+const printHello = createFunctionPrinter('hello');
+console.log(printHello())
+console.log(printHello())
+console.log(printHello())
+// => should console.log('hello');
 
 
 /** CHALLENGE 3 */
@@ -28,7 +35,7 @@ function outer() {
   let counter = 0; // this variable is outside incrementCounter's scope
   function incrementCounter () {
     counter ++;
-    console.log('counter', counter);
+    return counter
   }
   return incrementCounter;
 }
@@ -40,88 +47,166 @@ const jasCounter = outer();
 // Before your do, guess what will be logged from each function call.
 
 // /*** Uncomment these to check your work! ***/
-// willCounter();
-// willCounter();
-// willCounter();
-
-// jasCounter();
-// willCounter();
+console.log(willCounter())
+console.log(willCounter())
+console.log(willCounter())
+console.log(jasCounter())
+console.log(willCounter())
 
 
 function addByX(x) {
-
+  let value = x;
+  return function addValue(x) {
+    value += x
+   return value;
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const addByTwo = addByX(2);
-// addByTwo(1); // => should return 3
-// addByTwo(2); // => should return 4
-// addByTwo(3); // => should return 5
+const addByTwo = addByX(2);
+// console.log(addByTwo(1)) 
+// => should return 3
+// console.log(addByTwo(2)) 
+// => should return 4
+// console.log(addByTwo(3)) 
+// => should return 5
 
-// const addByThree = addByX(3);
-// addByThree(1); // => should return 4
-// addByThree(2); // => should return 5
+const addByThree = addByX(3);
+// console.log(addByThree(1)) 
+// => should return 4
+console.log(addByThree(2)) 
+// => should return 5
 
-// const addByFour = addByX(4);
-// addByFour(4); // => should return 8
-// addByFour(5); // => should return 9
+const addByFour = addByX(4);
+// console.log(addByFour(4)) 
+// => should return 8
+console.log(addByFour(5)) 
+// => should return 9
 
 
 /** CHALLENGE 4 */
+// CHALLENGE 4
 function once(func) {
+  let output = 0; 
+ 
+  return function(value) {
+    if(output > 0) return output;
+
+    output = func(value)
+    return output;
+  }
 
 }
 
 // /*** Uncomment these to check your work! ***/
-// const onceFunc = once(addByTwo);
-// console.log(onceFunc(4));  // => should log 6
-// console.log(onceFunc(10));  // => should log 6
-// console.log(onceFunc(9001));  // => should log 6
+const onceFunc = once(addByTwo);
+console.log(onceFunc(4));  // => should log 6
+console.log(onceFunc(10));  // => should log 6
+console.log(onceFunc(9001));  // => should log 6
+
 
 
 /** CHALLENGE 5 */
 function after(count, func) {
+  
+  return function() {
+    count--;
+    if(count === 0) {
+      return func();
+    } else {
+      return;
+    }
+  }
 
 }
 
 // /*** Uncomment these to check your work! ***/
-// const called = function() { console.log('hello') };
-// const afterCalled = after(3, called);
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => 'hello' is printed
+const called = function() { 
+  console.log('hello') 
+};
+const afterCalled = after(3, called);
+afterCalled() 
+// => nothing is printed
+afterCalled() 
+// => nothing is printed
+afterCalled() 
+// => 'hello' is printed
 
 
 /** CHALLENGE 6 */
-function delay(func, wait) {
-
+function delay(func) {
+  return function(wait, ...params) {
+    setTimeout(function(){
+      return func(...params)
+    }, wait)
+  }
+  
 }
+
+const callDelay = delay((number) => console.log(number))
+// callDelay(1000, "ok")
+// callDelay(2000, "ok 2")
+// callDelay(3000, "ok 3")
+
+// another way to implement without closure 
+const delay2 = function(cb, wait, ...params) {
+    setTimeout(function(){
+      cb(...params)
+    }, wait)
+}
+
+const sayHello = function(name) { console.log(name) }
+
+// delay2(sayHello, 4000, "Carlos")
 
 
 /** CHALLENGE 7 */
 function rollCall(names) {
+  let counter = -1;
 
+  return function() {
+    if(counter < names.length -1) {
+      counter++
+      return names[counter];
+    } else {
+      return "Everyone accounted for"
+    }
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
-// rollCaller() // => should log 'Victoria'
-// rollCaller() // => should log 'Juan'
-// rollCaller() // => should log 'Ruth'
-// rollCaller() // => should log 'Everyone accounted for'
+const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
+console.log(rollCaller()) 
+// => should log 'Victoria'
+console.log(rollCaller()) 
+// => should log 'Juan'
+console.log(rollCaller()) 
+// => should log 'Ruth'
+console.log(rollCaller()) 
+// => should log 'Everyone accounted for'
 
 
 /** CHALLENGE 8 */
 function saveOutput(func, magicWord) {
+  let output = {};
 
+  return function(arg) {
+    if(arg === magicWord) {
+      return output
+    }
+
+    return output[arg] = func(arg);
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const multiplyBy2 = function(num) { return num * 2; };
-// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-// console.log(multBy2AndLog(2)); // => should log 4
-// console.log(multBy2AndLog(9)); // => should log 18
-// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+const multiplyBy2 = function(num) { return num * 2; };
+
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+
+console.log(multBy2AndLog(2)); // => should log 4
+console.log(multBy2AndLog(9)); // => should log 18
+console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
 
 
 /** CHALLENGE 9 */
