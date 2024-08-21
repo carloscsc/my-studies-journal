9d1d39ed8e86534926c2a43bd4bf5a8a8cb27c6b
Carlos Sabo Cantanzaro <carlos.azarro@gmail.com>
2024-08-21 12:45:01 -0300

    add studies about vanilla js

---
+++ _miscellaneous/frontend_masters/vanilla_js/dom-api.js
@@ -0,0 +1,7 @@
+/** HTMLCollections don't have all the modern Array interface such
+ * filter, map, ect.. so use Array.from()
+ */
+// const elements =  Array.from(document.getElementsByClassName('test')) 
+// const newElements = elements.filter(e => e.tagName === "LI")
+// console.log(newElements)
+
