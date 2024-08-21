8e976396909144f0648a841c52ef402bf7eb0a7d
Carlos Sabo Cantanzaro <carlos.azarro@gmail.com>
2024-08-21 12:54:51 -0300

    Finish setup of project coffe from Vanilla JS - frontend masters

--- _miscellaneous/frontend_masters/vanilla_js/app.js
+++
@@ -1,7 +0,0 @@
-/** HTMLCollections don't have all the modern Array interface such
- * filter, map, ect.. so use Array.from()
- */
-// const elements =  Array.from(document.getElementsByClassName('test')) 
-// const newElements = elements.filter(e => e.tagName === "LI")
-// console.log(newElements)
-
