3f8d1ab8379c69aab48e970d37c4c88b6698d010
Carlos Sabo Cantanzaro <carlos.azarro@gmail.com>
2024-08-23 19:27:05 -0300

    add studies about route

--- _miscellaneous/frontend_masters/vanilla_js/coffeemasters-vanilla/services/Menu.js
+++ _miscellaneous/frontend_masters/vanilla_js/coffeemasters-vanilla/services/Menu.js
@@ -1,6 +1,6 @@
 import API from './API.js';
 
 export async function loadData() {
-    const data = await API.fetchMenu();
-    app.store.menu = data;
+  const data = await API.fetchMenu();
+  app.store.menu = data;
 }
--- _miscellaneous/frontend_masters/vanilla_js/coffeemasters-vanilla/services/Router.js
+++ _miscellaneous/frontend_masters/vanilla_js/coffeemasters-vanilla/services/Router.js
@@ -1,7 +1,60 @@
 const Router = {
-  init: () => {},
+  init: () => {
+    document.querySelectorAll('a.navlink').forEach((a) => {
+      a.addEventListener('click', (event) => {
+        event.preventDefault();
+        const link = new URL(event.target.href).pathname;
+        Router.go(link);
+      });
+    });
+
+    // Event handle for URL Changes
+    window.addEventListener('popstate', (event) => {
+      Router.go(event.state.route, false);
+    });
+
+    // Check the initial URL
+    Router.go(location.pathname);
+  },
+
   go: (route, addToHistory = true) => {
-    console.log('Init');
+    if (addToHistory) {
+      history.pushState({ route }, null, route);
+    }
+
+    let pageElement = null;
+
+    switch (route) {
+      case '/':
+        pageElement = document.createElement('h1');
+        pageElement.textContent = 'Home Page';
+
+        break;
+
+      case '/order':
+        pageElement = document.createElement('h1');
+        pageElement.textContent = 'Order Page';
+
+        break;
+
+      default:
+        if (route.startsWith('products-')) {
+          pageElement = document.createElement('h1');
+          pageElement.textContent = 'Details Page';
+
+          const paramId = route.substring(route.lastIndexOf('-') + 1);
+
+          pageElement.dataset.id = paramId;
+        }
+    }
+
+    if (pageElement) {
+      const main = document.querySelector('main');
+      main.innerHTML = '';
+      main.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
   },
 };
 
