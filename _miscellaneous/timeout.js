c8e7304f4a4b91cbf85d1776c89f723eb992f750
Carlos Sabo Cantanzaro <carlos.azarro@gmail.com>
2024-08-23 19:28:28 -0300

    Reformat timout file

--- _miscellaneous/timeout.js
+++ _miscellaneous/timeout.js
@@ -1,13 +1,13 @@
-// Example with setTimeout and setInterval 
+// Example with setTimeout and setInterval
 
 let num = 0;
-const logId = setInterval(()=> {
-  console.log(++num) // log 1-10
-}, 100)
+const logId = setInterval(() => {
+  console.log(++num); // log 1-10
+}, 100);
 
-setTimeout(()=> {
-  clearInterval(logId)
-}, 1100)
+setTimeout(() => {
+  clearInterval(logId);
+}, 1100);
 
 console.log('start');
 setTimeout(() => {
