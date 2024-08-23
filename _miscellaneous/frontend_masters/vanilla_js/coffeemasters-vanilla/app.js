b9d6f73da0fdf016974cc227740a4de5cc21cea5
Carlos Sabo Cantanzaro <carlos.azarro@gmail.com>
2024-08-23 16:41:32 -0300

    add router file

--- _miscellaneous/frontend_masters/vanilla_js/coffeemasters-vanilla/app.js
+++ _miscellaneous/frontend_masters/vanilla_js/coffeemasters-vanilla/app.js
@@ -1,10 +1,13 @@
 import Store from './services/Store.js';
 import API from './services/API.js';
-import { loadData } from "./services/Menu.js";
+import { loadData } from './services/Menu.js';
+import Router from './services/Router.js';
 
-window.app = {} // add a global variable attached to the window 
+window.app = {}; // add a global variable attached to the window
 app.store = Store;
+app.router = Router;
 
-window.addEventListener("DOMContentLoaded", () => {
-    loadData()
+window.addEventListener('DOMContentLoaded', () => {
+  app.router.init();
+  loadData();
 });
---
+++ _miscellaneous/frontend_masters/vanilla_js/coffeemasters-vanilla/services/Router.js
@@ -0,0 +1,8 @@
+const Router = {
+  init: () => {},
+  go: (route, addToHistory = true) => {
+    console.log('Init');
+  },
+};
+
+export default Router;
