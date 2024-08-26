43ce5ed51d4242de0d9846e92543f16d6a99d227
Carlos Sabo Cantanzaro <carlos.azarro@gmail.com>
2024-08-26 12:53:49 -0300

    Add more studies about docker

--- _miscellaneous/fc-developer/fc-devops-docker/node/index.js
+++ _miscellaneous/fc-developer/fc-devops-docker/node/index.js
@@ -1,24 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database:'nodedb'
-};
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// const config = {
+//   host: 'db',
+//   user: 'root',
+//   password: 'root',
+//   database: 'nodedb',
+// };
+// const mysql = require('mysql');
+// const connection = mysql.createConnection(config);
 
-const sql = `INSERT INTO people(name) values('Wesley')`
-connection.query(sql)
-connection.end()
+// const sql = `INSERT INTO people(name) values('Wesley')`;
+// connection.query(sql);
+// connection.end();
 
+// app.get('/', (req, res) => {
+//   res.send('<h1>Full Cycle</h1>');
+// });
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+// app.listen(port, () => {
+//   console.log('Rodando na porta ' + port);
+// });
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!!');
+});
+
+app.listen(port, () => {
+  console.log('Run at port' + port);
+});
