001cb609cddda5cceee53d8e21b6a2134dfd3533
Carlos Sabo Cantanzaro <carlos.azarro@gmail.com>
2024-09-18 21:22:55 -0300

    add files from api-tools

---
+++ _miscellaneous/fc-developer/cumunicacao_sistemas/api-tools-skeleton/config/autoload/local.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'db' => [
+        'adapters' => [
+            'Sqlite' => [
+                'database' => 'test.sqlite',
+                'driver' => 'PDO_Sqlite',
+                'username' => 'carlos@metropolitanafm.com.br',
+                'password' => '#Adm2024#',
+            ],
+        ],
+    ],
+];
